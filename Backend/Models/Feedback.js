3554873316faebd2344c67e185653d573da85bc8
Pramodathukorala <155905078+Pramodathukorala@users.noreply.github.com>
2024-08-25 00:25:58 +0530

    Update Feedback.js
    feedback model

---
+++ Backend/Models/Feedback.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const feedbackSchema = schema({
+    cusID: {
+        type: String,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    phone_number:{
+        type:String,
+        required: true,
+    }
+    email: {
+        type: String,
+        required: true,
+    },
+    employee:{
+        type: String,
+        required: true,
+    },
+    date_of_service:{
+        type: Date,
+        required: true,
+    },
+    message:{
+        type: String,
+        required: true,
+    },
+    star_rating:{
+        type:Number,
+        required: true,
+    },
+}
+    
+);
