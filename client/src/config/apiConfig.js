0c188b6931c6ae0e7c94b38858b635cc64ecf412
Niran B G S H <127426411+HasinduNiran@users.noreply.github.com>
2025-03-19 17:46:06 +0530

    create apiconfig

---
+++ client/src/config/apiConfig.js
@@ -0,0 +1,10 @@
+// apiconfig.js
+const API_CONFIG = {
+  BASE_URL: "http://localhost:7001", // Base URL for your backend
+  ENDPOINTS: {
+    PROMOTIONS: "/api/promotions",
+    INVENTORY: "/api/inventory", // Added inventory endpoint
+  },
+};
+
+export default API_CONFIG;
