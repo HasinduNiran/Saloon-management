a738ac6fc9fb1e4bc6ea97ef5c3bfc0aea27733e
Pramodathukorala <155905078+Pramodathukorala@users.noreply.github.com>
2025-03-19 14:19:25 +0530

    Update appoimentModel.js

--- api/models/appoimentModel.js
+++ api/models/appoimentModel.js
@@ -12,7 +12,7 @@ const AppoiCounter = mongoose.model('AppoiCounter', counterSchema);
 // Appointment Schema
 const appointmentSchema = mongoose.Schema(
     {
-        CusID: {
+        Username: {
             type: String,
         },
         appoi_ID: {
