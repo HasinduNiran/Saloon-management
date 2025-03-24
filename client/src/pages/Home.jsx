7ba541c4f20d4c955477d57cea17d5a70c8e408a
malakasadeep <151869451+malakasadeep@users.noreply.github.com>
2025-03-24 22:34:04 +0530

    Refactor CORS configuration and enhance Home component to display user information

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 function Home() {
+  const { user } = useSelector((state) => state.auth);
   return (
-    <div>Home</div>
-  )
+    <>
+      <div>Home</div>
+      <p>{user.name}</p>
+      <p>{user._id}</p>
+    </>
+  );
 }
 
-export default Home
+export default Home;
